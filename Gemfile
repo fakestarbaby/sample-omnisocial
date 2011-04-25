2b5533cd86db05ffdd9a9eadc7c8d148f74eafe7
fakestarbaby <fakestarbaby@gmail.com>
2011-04-26 08:35:52 +0900

    初回コミット。

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'omnisocial'
+gem 'sqlite3-ruby', :require => 'sqlite3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleOmnisocial::Application.config.secret_token = '640019a31afc165aec76fae2a87d7b417fbbe6a832c69372d576e5b2b1874d76de3f22ed704befd645bb20c89ada7990950b5ee992ecee6fd2e0e3ede21f5a4c'
